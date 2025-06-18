c3a3348c2d0041d6416c1737b48725280a569014
dggupta <divyanshgupta96302@gmail.com>
2025-06-18 19:49:58 +0530

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -34,7 +34,7 @@ const router = createBrowserRouter([
 ])
 ReactDom.createRoot(document.getElementById('root')).render(
   <StrictMode>
-  <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_AUTH_CLIENT_ID}>...
+  <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_AUTH_CLIENT_ID}>
   <Header/>
   <Toaster/>
   <RouterProvider router={router} />
